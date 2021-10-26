0972bcfd422428dd584aabb4b8f83fb5e7935d7c
Isaac640 <73839720+Isaac640@users.noreply.github.com>
2021-10-26 19:34:39 +0200

    Add files via upload
    commit a mano

---
+++ Isaac Github/src/isaac/github/IsaacGithub.java
@@ -0,0 +1,16 @@
+package isaac.github;
+
+/**
+ *
+ * @author DAM103
+ */
+public class IsaacGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
